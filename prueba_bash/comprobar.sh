bf7237c4baec21bd5d9963d991a742ed5a3b38c1
VicentFer01 <vicfermor22@gmail.com>
2026-09-17 10:16:11 +0200

    ejercicio b1

---
+++ prueba_bash/comprobar.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+if (num % 2 != 0) {
+	El numero es par } else {
+	El numero no es par}
---
+++ prueba_bash/saludo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+if ( !nombre -f ) {
+	echo"Hola" #nombre" 
+} else {
+	echo"Debes indicar un nombre"																			
+}
